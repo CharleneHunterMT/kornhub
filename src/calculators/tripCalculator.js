7d62ffca62fb8dea2d3cbc713b127592f6107f00
Charlene Hunter <charlenehunter1985@gmail.com>
2020-05-06 16:16:16 +0100

    Doing parse Int

--- src/calculators/tripCalculator.js
+++ src/calculators/tripCalculator.js
@@ -1,6 +1,7 @@
 export default (geese, corn) => {
   console.log('geese: ' + geese + ' corn: ' +corn)
-
+ geese = parseInt(geese);
+ corn = parseInt(corn);
 
   if (corn > 2 && geese > 2) {
     return undefined;
@@ -9,16 +10,16 @@ export default (geese, corn) => {
   let twoGeeseOneCorn = ["C", "", "G", "C", "G", "", "C"];
   let oneGeeseOneCorn = ["G", "", "C"];
 
-  if (geese == 1 && corn == 1) {
+  if (geese === 1 && corn === 1) {
     return oneGeeseOneCorn.length;
   }
-  if (geese == 1 && corn == 2) {
+  if (geese === 1 && corn === 2) {
     return twoGeeseOneCorn.length;
   }
-  if (geese == 2 && corn == 1) {
+  if (geese === 2 && corn === 1) {
     return twoCornOneGoose.length;
   }
-  if (corn == 0) {
+  if (corn === 0) {
     let geeseArray = [];
     for (let i = 0; i < geese - 1; i++) {
       geeseArray.push("G", "");
@@ -26,7 +27,7 @@ export default (geese, corn) => {
     geeseArray.push("G");
     return geeseArray.length;
   }
-  if (geese == 0) {
+  if (geese === 0) {
     let cornArray = [];
     for (let i = 0; i < corn - 1; i++) {
       cornArray.push("C", "");
